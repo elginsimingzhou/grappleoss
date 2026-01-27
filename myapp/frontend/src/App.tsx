0c1acc7388d370137bcf0a8ce3f7979a20a4626e
Si Ming Zhou <elginsi2000@gmail.com>
2026-01-27 08:59:27 +0800

    added detail node

--- myapp/frontend/src/App.tsx
+++ myapp/frontend/src/App.tsx
@@ -1,17 +1,6 @@
-import { useEffect, useState } from "react";
 import GraphUI from "./components/graphs_ui/GraphUI";
 
-type HelloResponse = { message: string };
-
 export default function App() {
-  const [msg, setMsg] = useState<string>("Loading....");
-
-  useEffect(() => {
-    fetch("/api/hello?name=Elgin")
-      .then((r) => r.json() as Promise<HelloResponse>)
-      .then((data) => setMsg(data.message))
-      .catch(() => setMsg("Error fetching message"));
-  }, []);
 
   return (
     <div style={{ padding: 24, fontFamily: "system-ui" }}>
